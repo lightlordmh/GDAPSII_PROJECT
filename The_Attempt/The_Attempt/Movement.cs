b560082b36892fd7062deb046c6522d37fa1a161
lightlordmh <lightlordmh@hotmail.com>
2016-03-18 16:08:32 -0400

    Added Comments to explain the input and Movement Classes

--- The_Attempt/The_Attempt/Movement.cs
+++ The_Attempt/The_Attempt/Movement.cs
@@ -8,38 +8,63 @@ namespace The_Attempt
 {
     public class Movement
     {
+        //Attribute
         //acts as differnce in speed
         private float scale = 3;
-
-
-
-        //for changing the scale
+        
+        //Property
+        //Allows Access to get and set the scale Attribute
         public float Scale
         {
             get { return scale; }
             set { scale = value; }
         }
-
+        /// <summary>
+        /// does a caluculation to move the vector2 left 
+        /// </summary>
+        /// <param name="i">Input Vector2 object for left Movement calculation</param>
+        /// <returns>A Vector2 with a new position based on the calculations made</returns>
         public Vector2 Left(Vector2 i)
         {
+            //Create a Vector2 object with a postions that moves the parameter Vector2 left
             Vector2 afterM = new Vector2(i.X - (1 * scale), i.Y);
+            //return the Vector2 with the new postion
             return afterM;
         }
+        /// <summary>
+        /// does a caluculation to move the vector2 right 
+        /// </summary>
+        /// <param name="i">Input Vector2 object for right Movement calculation</param>
+        /// <returns>A Vector2 with a new position based on the calculations made</returns>
         public Vector2 Right(Vector2 i)
         {
+            //Create a Vector2 object with a postions that moves the parameter Vector2 right
             Vector2 afterM = new Vector2(i.X + (1 * scale), i.Y);
+            //return the Vector2 with the new postion
             return afterM;
         }
-
-
+        /// <summary>
+        /// does a caluculation to move the vector2 up 
+        /// </summary>
+        /// <param name="i">Input Vector2 object for up Movement calculation</param>
+        /// <returns>A Vector2 with a new position based on the calculations made</returns>
         public Vector2 Up(Vector2 i)
         {
+            //Create a Vector2 object with a postions that moves the parameter Vector2 up
             Vector2 afterM = new Vector2(i.X, i.Y - (1 * scale));
+            //return the Vector2 with the new postion
             return afterM;
         }
+        /// <summary>
+        /// does a caluculation to move the vector2 down
+        /// </summary>
+        /// <param name="i">Input Vector2 object for down Movement calculation</param>
+        /// <returns>A Vector2 with a new position based on the calculations made</returns>
         public Vector2 Down(Vector2 i)
         {
+            //Create a Vector2 object with a postions that moves the parameter Vector2 down
             Vector2 afterM = new Vector2(i.X, i.Y + (1 * scale));
+            //return the Vector2 with the new postion
             return afterM;
         }
     }
--- The_Attempt/The_Attempt/input.cs
+++ The_Attempt/The_Attempt/input.cs
@@ -7,46 +7,61 @@ namespace The_Attempt
     {
 
         /// <summary>
-        /// 
+        /// checks the current state of the keyboard then
+        /// does selected events based on the keyboard state
         /// </summary>
         /// <param name="curGameMap">Parameter to store the game's map</param>
-        public void Check(Map curGameMap) //current game map
+        public void Check(Map curGameMap)
         {
-            KeyboardState kbState;
-            kbState = Keyboard.GetState();
+            KeyboardState kbState = Keyboard.GetState(); //keyboardState object to store the current state of the keyboard
 
-            //Movement object
-            Movement inputMove = new Movement();
+            Movement inputMove = new Movement(); //Movement object to move the map object based on keyboard input
 
 
             //Each key that we might use
+            //Check if the leftshift key is being pressed
             if (kbState.IsKeyDown(Keys.LeftShift)) //Running if we have it
             {
+                //call and set inputMove's scale propety 
                 inputMove.Scale = 1.3F;
             }
+            //Check if the W key is being pressed
             if (kbState.IsKeyDown(Keys.W))
             {
+                //Create a Vector2 object given the current game maps position.
                Vector2 bMM = curGameMap.MapPos;
+                //change the current game map's position to that of inputMove's down method given the vector2
                curGameMap.MapPos = inputMove.Down(bMM);
             }
+            //Check if the D key is being pressed
             if (kbState.IsKeyDown(Keys.D))
             {
+                //Create a Vector 2 object given the current game maps position
                 Vector2 bMM = curGameMap.MapPos;
+                //Change the current game map's position to thtat of the inputMove's left method given the Vector2
                 curGameMap.MapPos = inputMove.Left(bMM);
             }
+            //Check if the S key is being pressed
             if (kbState.IsKeyDown(Keys.S))
             {
+                //Create a Vector2 object give the current game maps position
                 Vector2 bMM = curGameMap.MapPos;
+                //chagne the current game mp's position to that of the inputMove's up method given the Vector2
                 curGameMap.MapPos = inputMove.Up(bMM);
             }
+            //Check if the A key is being pressed
             if (kbState.IsKeyDown(Keys.A))
             {
+                //Create a Vector2 object given the current game maps position
                 Vector2 bMM = curGameMap.MapPos;
+                //change the current game map's positon to that of the inputMove's right method give the Vector2
                 curGameMap.MapPos = inputMove.Right(bMM);
             }
+            //check if the space key is being pressed
+            
             if (kbState.IsKeyDown(Keys.Space)) //Flashlight
             {
-
+                // for future implementation
             }
 
             //return it
